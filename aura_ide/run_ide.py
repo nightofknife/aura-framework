899adf62414c3986d4d0b7b02f4d4d38b242bd9c
nightofknife <3313899887@qq.com>
2025-09-15 20:29:58 +0800

    删掉一些无用的方案包

--- aura_ide/run_ide.py
+++ aura_ide/run_ide.py
@@ -18,20 +18,6 @@ from aura_ide.main_window import MainWindow
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    # --- 样式表加载 (更健壮的方式) ---
-    # 2. 构建一个相对于当前文件位置的绝对路径
-    #    Path(__file__) -> 当前文件的路径 (run_ide.py)
-    #    .parent -> 获取其所在的目录
-    #    / "resources/solarized_dark.qss" -> 拼接目标文件
-    # qss_path = Path(__file__).parent / "resources/solarized_dark.qss"
-
-    # try:
-    #     with open(qss_path, "r", encoding="utf-8") as f:  # 建议加上 encoding
-    #         app.setStyleSheet(f.read())
-    # except FileNotFoundError:
-    #     print(f"Stylesheet not found at: {qss_path}")
-    # # --- 结束 ---
-
     window = MainWindow()
     window.show()
 
